aedcf8132aa17e6acefb56afea7752b511a21892
vinceMburu123 <judevinny@gmail.com>
2024-09-04 21:03:07 +0300

    Answers plp python

---
+++ user_input.py
@@ -0,0 +1,11 @@
+
+name = input("What is your name? ")
+
+
+age = input("What is your age? ")
+
+# Ask the user for their location and store it in a variable
+location = input("Where do you live? ")
+
+# Print out a personalized message using the user's input
+print(f"Hello {name}, you are {age} years old and live in {location}.")
